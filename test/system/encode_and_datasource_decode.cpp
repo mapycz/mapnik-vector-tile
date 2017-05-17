2f42a4dfbab8db9baa6320d579329f51136742aa
Jiri Drbalek <jiri.drbalek@firma.seznam.cz>
2017-05-17 12:57:42 +0000

    adapt to szn-mapnik

--- test/system/encode_and_datasource_decode.cpp
+++ test/system/encode_and_datasource_decode.cpp
@@ -3,13 +3,8 @@
 // mapnik
 #include <mapnik/geometry.hpp>
 #include <mapnik/version.hpp>
-#if MAPNIK_VERSION >= 300100
-#include <mapnik/geometry/strategy.hpp>
-#include <mapnik/geometry/transform.hpp>
-#else
 #include <mapnik/geometry_strategy.hpp>
 #include <mapnik/geometry_transform.hpp>
-#endif
 
 // mapnik-vector-tile
 #include "vector_tile_geometry_encoder_pbf.hpp"
